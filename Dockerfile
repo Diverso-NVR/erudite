c643762daa9e1012cdcdac253f8618dca7c27564
kuderr <dakudryavcev@gmail.com>
2020-11-20 17:10:25 +0300

    deploy stuff, flake8+black refactor

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
 FROM python:3.8.5
 
-COPY . /equipment_portal /equipment_portal /
-COPY . /requirements.txt /
+COPY . /erudite
+COPY . /requirements.txt
 
 RUN pip install -r requirements.txt
 
 EXPOSE 5010
 
-
-CMD ["uvicorn", "equipment_portal.main:app", "--host", "0.0.0.0", "--port", "5010"]
+CMD ["uvicorn", "erudite.main:app", "--host", "0.0.0.0", "--port", "5010"]
---
+++ erudite/routers/equipment.py
@@ -0,0 +1,78 @@
+from fastapi import APIRouter
+import logging
+
+from db.models import Equipment, db
+
+router = APIRouter()
+
+logger = logging.getLogger("equipment_portal")
+
+
+@router.get("/equipment")
+async def list_equipments():
+    """Достаем все equipment"""
+
+    equipment_list = []
+    async for equipment in db.equipment.find():
+        equipment_list.append(Equipment(**equipment))
+    try:
+
+        if len(equipment_list) == 0:
+            logger.info("No items")
+            return {}
+        else:
+            logger.info("Equipment in the database: {equipment_list}")
+            return {"equipment": equipment_list}
+    except Exception:
+        logger.error("Wrong data in the database")
+
+
+@router.get("/equipment/{equipment_id}")
+async def find_equipment(equipment_id: int):
+    """Достаем обьект equipment из бд"""
+
+    try:
+        equipment = await db.equipment.find_one({"_id": equipment_id})
+        if equipment:
+            logger.info(f"Equipment {equipment_id}: {Equipment(**equipment)}")
+            return Equipment(**equipment)
+        else:
+            logger.info("This equipment is not found")
+            return {}
+    except Exception:
+        logger.error("Wrong data in the database")
+
+
+@router.post("/equipment")
+async def create_equipment(equipment: Equipment):
+    """Добавляем обьект equipment в бд"""
+
+    try:
+        await db.equipment.insert_one(equipment.dict(by_alias=True))
+        logger.info(f"Equipment with id: {equipment.id}  -  added to the database")
+    except Exception:
+        logger.error(
+            f"Equipment with id: {equipment.id}  -  already exists in the database"
+        )
+    return {"equipment": equipment}
+
+
+@router.delete("/equipment/{equipment_id}")
+async def delete_equipment(equipment_id: int):
+    """Удаляем обьект equipment из бд"""
+
+    await db.equipment.delete_one({"_id": equipment_id})
+    logger.info(f"Equipment with id: {equipment_id}  -  deleted from the database")
+
+
+@router.put("/equipment/{equipment_id}")
+async def update_equipment(equipment_id: int, new_values_dict: dict):
+    """Обновляем/добавляем поле/поля в equipment в бд"""
+
+    try:
+        await db.equipment.update_one({"_id": equipment_id}, {"$set": new_values_dict})
+        logger.info(
+            f"Equipment with id: {equipment_id}  -  updated"
+        )  # Если ключа нет в обьекте, то будет добавлена новая пара ключ-значение к этому обьекту
+    except Exception:
+        logger.error("Element with this id not found")
