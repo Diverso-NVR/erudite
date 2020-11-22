04f2584ac76aec2cb570b40366fde2915428173d
danil <danil.mirtalibov.27@mail.ru>
2020-11-22 15:10:59 +0300

    room router is fixed

--- erudite/routers/rooms.py
+++ erudite/routers/rooms.py
@@ -47,63 +47,86 @@ async def find_room(room_id: str):
 async def create_room(room: Room):
     """Добавляем обьект room в бд"""
 
-    try:
-        await db.rooms.insert_one(room.dict(by_alias=True))
+    if await rooms_collection.find_one({"name": room.name}):
+        logger.info(f"Room with name: {room.name}  -  already exists in the database")
+        return {}
+    else:
+        room_added = await rooms_collection.insert_one(room.dict(by_alias=True))
+        new_room = await rooms_collection.find_one({"_id": room_added.inserted_id})
         logger.info(f"Room: {room.name}  -  added to the database")
-    except Exception:
-        logger.error(f"Room: {room.name}  -  already exists in the database")
 
-    return {"room": room}
+        return {"room": new_room.__repr__()}
 
 
-@router.delete("/room/{room_name}")
-async def delete_room(room_name: str):
+@router.delete("/room/{room_id}")
+async def delete_room(room_id: str):
     """Удаляем обьект room из бд"""
 
-    await db.rooms.delete_one({"_id": room_name})
-    logger.info(f"Room: {room_name}  -  deleted from the database")
+    try:
+        if await rooms_collection.find_one({"_id": ObjectId(room_id)}):
+            await rooms_collection.delete_one({"_id": ObjectId(room_id)})
+            logger.info(f"Room: {room_id}  -  deleted from the database")
+        else:
+            logger.info(f"Room: {room_id}  -  not found in the database")
+            return "Rooom not found"
+    except Exception:
+        logger.error("Wrong ID")
+        return "Wrong ID"
 
 
-@router.patch("/room/{room_name}")
-async def patch_room(room_name: str, new_values_dict: dict):
+@router.patch("/room/{room_id}")
+async def patch_room(room_id: str, new_values: dict) -> str:
     """Обновляем/добавляем поле/поля в room в бд"""
 
     try:
-        await db.rooms.update_one({"_id": room_name}, {"$set": new_values_dict})
-        logger.info(
-            f"Room: {room_name}  -  pached"
-        )  # Если ключа нет в обьекте, то будет добавлена новая пара ключ-значение к этому обьекту
+        if await rooms_collection.find_one({"_id": ObjectId(room_id)}):
+            await rooms_collection.update_one({"_id": ObjectId(room_id)}, {"$set": new_values})
+            logger.info(
+                f"Room: {room_id}  -  pached"
+            )  # Если ключа нет в обьекте, то будет добавлена новая пара ключ-значение к этому обьекту
+            return "done"
+        else:
+            logger.info(f"Room: {room_id}  -  not found in the database")
+            return "Rooom not found"
     except Exception:
-        logger.error("Element with this name not found")
+        logger.error("Wrong Id")
+        return "Wrong Id"
 
 
-@router.put("/room/{room_name}")
-async def update_room(room_name: str, new_values: Room):
+@router.put("/room/{room_id}")
+async def update_room(room_id: str, new_values: dict) -> str:
     """Обновляем все поле/поля в room в бд"""
 
-    # try:
-    await db.rooms.delete_one({"_id": room_name})
-    await db.rooms.insert_one(new_values.dict(by_alias=True))
-    logger.info(
-        f"Room: {room_name}  -  updated"
-    )  # Если ключа нет в обьекте, то будет добавлена новая пара ключ-значение к этому обьекту
-    # except Exception:
-    # logger.error("Element with this name not found")
+    try:
+        if await rooms_collection.find_one({"_id": ObjectId(room_id)}):
+            await rooms_collection.delete_one({"_id": ObjectId(room_id)})
+            await rooms_collection.insert_one({"_id": ObjectId(room_id)})
+            await rooms_collection.update_one({"_id": ObjectId(room_id)}, {"$set": new_values})
+            logger.info(
+                f"Room: {room_id}  -  updated"
+            )  # Если ключа нет в обьекте, то будет добавлена новая пара ключ-значение к этому обьекту
+            return "done"
+        else:
+            logger.info(f"Room: {room_id}  -  not found in the database")
+            return "Rooom not found"
+    except Exception:
+        logger.error("Wrong Id")
+        return "Wrong Id"
 
 
-@router.get("/room/{room_name}/equipment")
-async def list_room_equipments(room_name: str):
+@router.get("/room/{room_id}/equipment")
+async def list_room_equipments(room_id: str):
     """Достаем все equipment из конкретной комнаты"""
 
     equipment_list = []
     try:
-        async for equipment in db.equipment.find({"room_name": room_name}):
-            equipment_list.append(Equipment(**equipment))
+        async for equipment in db.equipment.find({"room_id": room_id}):
+            equipment_list.append(equipment)
         if len(equipment_list) == 0:
             logger.info("No equipment in the room found")
             return {}
         else:
-            logger.info(f"Equipment in the room {room_name}: {equipment_list}")
+            logger.info(f"Equipment in the room {room_id}: {equipment_list}")
             return equipment_list
     except Exception:
         logger.error("Wrong data in the database")
