ea2e5897e3ee52d7e0211f7ce90847f2b874ca65
kuderr <dakudryavcev@gmail.com>
2020-11-26 02:02:13 +0300

    disciplines fetch by course_code or id

--- erudite/core/routers/disciplines.py
+++ erudite/core/routers/disciplines.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 import logging
+from typing import Optional
+
+from bson.objectid import ObjectId
 
 from ..db.models import Discipline, db, mongo_to_dict
 
@@ -11,11 +14,35 @@ disciplines_collection = db.get_collection("disciplines")
 
 
 @router.get("/disciplines")
-async def list_disciplines():
-    """Достаем все disciplines"""
-    return [
-        mongo_to_dict(discipline) async for discipline in disciplines_collection.find()
-    ]
+async def find_discipline(course_code: Optional[str] = None):
+    """Достаем обьект discipline из бд"""
+
+    if course_code is None:
+        return [
+            mongo_to_dict(discipline)
+            async for discipline in disciplines_collection.find()
+        ]
+
+    discipline = await disciplines_collection.find_one({"course_code": course_code})
+    if discipline:
+        logger.info(f"Discipline {course_code}: {discipline}")
+        return mongo_to_dict(discipline)
+    else:
+        logger.info("This discipline is not found")
+        return {}
+
+
+@router.get("/disciplines/{discipline_id}")
+async def find_discipline(discipline_id: str):
+    """Достаем обьект discipline из бд"""
+
+    discipline = await disciplines_collection.find_one({"_id": ObjectId(discipline_id)})
+    if discipline:
+        logger.info(f"Discipline {discipline_id}: {discipline}")
+        return mongo_to_dict(discipline)
+    else:
+        logger.info("This discipline is not found")
+        return {}
 
 
 @router.post("/disciplines", status_code=201)
